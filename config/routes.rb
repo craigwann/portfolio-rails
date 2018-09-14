6cee2173e6a2be2641e10c613cb303770a0ed344
craigwann <craigwann@me.com>
2018-09-14 12:04:11 -0700

    add gem rails_admin and install

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # devise_for :users
   root :to => 'projects#index'
   resources :projects
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        passwords: 'users/passwords',
+        registrations: 'users/registrations'
+    }
+
 
 
 end
